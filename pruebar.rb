34848658e0ffbbaec6a2fb74bd95d12aadb5f8c6
evelyn <astudillobarra.evelyn@gmail.com>
2018-06-21 16:29:06 -0400

    other commit 2

--- pruebar.rb
+++ pruebar.rb
@@ -1,59 +1,59 @@
 
-  file = File.open('alumnos.csv', 'r')
-  data = file.readlines.map(&:chomp)
-  file.close
-
-
-
-  def promedio
-  file = File.open('alumnos.csv', 'r')
-  data = file.readlines.map(&:chomp)
-  suma = 0
-  data.each do |elem|
-  suma += elem.split(', ')[1].to_i
-  end
-   puts suma
-  end
-promedio
-
-def inasistencias(letter)
-  file = File.open('alumnos.csv', 'r')
-  data = file.readlines.map(&:chomp)
-  data.each do |let|
-  let.include?(letter)
-end
-p inasistencias('A')
-end
-
-def aprobados
     file = File.open('alumnos.csv', 'r')
-  data = file.readlines.map(&:chomp)
-
-end
-
-
-puts 'Opciones'
-opcion = 0
-while opcion != 4
-  puts '1   Promedio de cada alumno'
-  puts '2   Inasistencias Totales'
-  puts '3   Alumnos Aprobados'
-  puts '4   Salir'
-  puts 'Ingrese su opcion'
- opcion = gets.chomp
-
-  puts case opcion
-when '1'
-  promedio(data)
+    data = file.readlines.map(&:chomp)
+    file.close
 
 
-when '2'
-  inasistencias(data)
 
-when '3'
-  aprobados(data)
-else
-  exit
+    def promedio
+    file = File.open('alumnos.csv', 'r')
+    data = file.readlines.map(&:chomp)
+    suma = 0
+    data.each do |elem|
+    suma += elem.split(', ')[1].to_i
+    end
+     puts suma
+    end
+  promedio
 
-end
-end
+  def inasistencias(letter)
+    file = File.open('alumnos.csv', 'r')
+    data = file.readlines.map(&:chomp)
+    data.each do |let|
+    let.include?(letter)
+  end
+  p inasistencias('A')
+  end
+
+  def aprobados
+      file = File.open('alumnos.csv', 'r')
+    data = file.readlines.map(&:chomp)
+
+  end
+
+
+  puts 'Opciones'
+  opcion = 0
+  while opcion != 4
+    puts '1 = Promedio de cada alumno'
+    puts '2 = Inasistencias Totales'
+    puts '3 = Alumnos Aprobados'
+    puts '4 = Salir'
+    puts 'Ingrese su opcion'
+   opcion = gets.chomp
+
+    puts case opcion
+  when '1'
+    promedio(data)
+
+
+  when '2'
+    inasistencias(data)
+
+  when '3'
+    aprobados(data)
+  else
+    exit
+
+    end
+   end
