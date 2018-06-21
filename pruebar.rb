15773370a1d129a18db94d17e765792afd550ebc
evelyn <astudillobarra.evelyn@gmail.com>
2018-06-21 12:55:55 -0400

    prom calification

--- pruebar.rb
+++ pruebar.rb
@@ -1,17 +1,31 @@
 
-file = File.open('alumnos.csv', 'r')
-data = file.readlines.map(&:chomp)
-file.close
-p data
-
-curso = data.map{ |elem| elem.split(', ')}
-p curso
+  file = File.open('alumnos.csv', 'r')
+  data = file.readlines.map(&:chomp)
+  file.close
+  
 
 
+  def promedio
+  file = File.open('alumnos.csv', 'r')
+  data = file.readlines.map(&:chomp)
+  suma = 0
+  data.each do |elem|
+  suma += elem.split(', ')[1].to_i
+  end
+   puts suma
+  end
+promedio
 
-
-#Hash[ta.each_slice(6).to_a]
-
+  #def count_word
+    #file = File.open('movies.txt', 'r')
+    #contenido = file.readlines.map(&:chomp)
+    #count = 0
+    #contenido.each do |line|
+    #count +=  line.split(' ').count
+    #end
+    #puts "hola acá esta la suma #{count}"
+  #end
+  #count_word
 
 
 
